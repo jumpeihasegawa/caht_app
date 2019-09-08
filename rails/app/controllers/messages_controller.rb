0d9f6506497aebac9c57a55ef4f0fe31a51a3c67
Jumpei Hasegawa <jumpei.peipei@gmail.com>
2019-09-08 17:42:58 +0900

    ルーティングを追加

--- rails/app/controllers/messages_controller.rb
+++ rails/app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
     def find_messages(talk_room_id)
     end
 
-    def rgister_message(message)
+    def create_message(message)
     end
 
     def delete_message(message_id)
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/login', to: 'logins#login'
+
+  post '/talkroom/create', to: 'talk_rooms#create_talk_room'
+  get  '/talkroom/find'  , to: 'talk_rooms#find_room_names'
+
+  get  'message/find(:talk_room_id)', to: 'messages#find_messages'
+  post 'message/create',              to: 'messages#create_message'
+  post 'message/delete',              to: 'messages#delete_message'
 end
