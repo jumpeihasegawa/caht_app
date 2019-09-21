e6c828c5a2d6d55b705a79ff5a21c9be52a7be91
Jumpei Hasegawa <jumpei.peipei@gmail.com>
2019-09-21 18:18:57 +0900

    トークルーム名を登録するAPIを作成

--- rails/app/controllers/talk_rooms_controller.rb
+++ rails/app/controllers/talk_rooms_controller.rb
@@ -1,5 +1,8 @@
 class TalkRoomsController < ApplicationController
     def create_talk_room
+        is_create = TalkRoom.create_talk_room(params[:room_name])
+
+        render json: is_create
     end
 
     def find_room_names
--- rails/app/models/talk_room.rb
+++ rails/app/models/talk_room.rb
@@ -1,2 +1,10 @@
 class TalkRoom < ApplicationRecord
+    def self.create_talk_room(room_name)
+        begin
+            self.create!(room_name: room_name)
+            return true
+        rescue
+            return false
+        end
+    end
 end
